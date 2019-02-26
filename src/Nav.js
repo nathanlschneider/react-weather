67d5eceaef81b7341b99f8172b5c8855a21fdef7
Nate Schneider <nschneider@mhgi.net>
2019-02-26 15:37:58 -0500

    added animation for zip menu

--- src/Nav.js
+++ src/Nav.js
@@ -1,43 +1,51 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { CSSTransitionGroup } from 'react-transition-group'; // ES6
+
 import './Nav.scss';
 
 export default class Nav extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: false };
-    this.handleClick = this.handleClick.bind(this);
-    this.handleMouseOver = this.handleMouseOver.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = { value: false };
+        this.handleClick = this.handleClick.bind(this);
+        this.handleMouseOver = this.handleMouseOver.bind(this);
+    }
 
-  handleClick() {
-    this.state.value ? this.setState({ value: false }) : this.setState({ value: true });
-  }
+    handleClick() {
+       this.setState({ value: !this.state.value });
+    }
 
-  handleMouseOver() {
-    this.setState({ value: true });
-  }
+    handleMouseOver() {
+        this.setState({ value: true });
+    }
 
-  render() {
-    return (
-      <nav className='nav'>
-        <div className='nav__logo'>Weather</div>
-        <div className='nav__links'>
-          <Link className='nav__link' to='/current'>
-            Current
-          </Link>
-          <Link className='nav__link' to='/forecast'>
-            Forecast
-          </Link>
-          <Link className='nav__link' to='/radar'>
-            Radar
-          </Link>
-          <span className='nav__link' onMouseOver={this.handleMouseOver} onClick={this.handleClick}>
-            Location <span style={{ color: 'magenta' }}>{this.props.zipCode}</span>
-          </span>
-        </div>
-        <div style={{ opacity: this.state.value ? 1 : 0 }}>{this.props.form}</div>
-      </nav>
-    );
-  }
+    render() {
+      let menu;
+  
+      this.state.value ? menu = this.props.form : menu = '';
+  
+        return (
+            <nav className='nav'>
+                <div className='nav__logo'>Weather</div>
+                <div className='nav__links'>
+                    <Link className='nav__link' to='/current'>
+                        Current
+                    </Link>
+                    <Link className='nav__link' to='/forecast'>
+                        Forecast
+                    </Link>
+                    <Link className='nav__link' to='/radar'>
+                        Radar
+                    </Link>
+                    <span className='nav__link' onClick={this.handleClick}>
+                        Location <span style={{ color: 'magenta' }}>{this.props.zipCode}</span>
+                    </span>
+                </div>
+                <CSSTransitionGroup transitionName='example' transitionEnterTimeout={500} transitionLeaveTimeout={300}>
+                    {menu}
+                </CSSTransitionGroup>
+            </nav>
+        );
+    }
 }
