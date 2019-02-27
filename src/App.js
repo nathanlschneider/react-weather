c54970c056fcbcd1146361fd37f1861102865acb
nathanlschneider <n.l.schneider@gmail.com>
2019-02-26 23:39:40 -0500

    menu styling

--- src/App.js
+++ src/App.js
@@ -7,67 +7,70 @@ import Forecast from './Forecast';
 import './App.scss';
 
 export default class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            zipCode: 49418,
-            value: null,
-            showMenu: false
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleClick = this.handleClick.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      zipCode: 49418,
+      value: null,
+      showMenu: false
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-    handleClick() {
-        this.setState({ showMenu: !this.state.showMenu });
-    }
+  handleClick() {
+    this.setState({ showMenu: !this.state.showMenu });
+  }
 
-    handleChange(event) {
-        this.setState({ value: event.target.value });
-    }
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
 
-    handleSubmit(event) {
-        this.setState({ zipCode: this.state.value });
-        this.setState({ showMenu: false });
-        event.preventDefault();
-    }
+  handleSubmit(event) {
+    this.setState({ zipCode: this.state.value });
+    this.setState({ showMenu: false });
+    event.preventDefault();
+  }
 
-    render() {
-        return (
-            <div className='app'>
-                <div className='app__container'>
-                    <header className='app__header'>
-                        <Nav
-                            zipCode={this.state.zipCode}
-                            form={
-                                this.state.showMenu ? (
-                                    <div className='nav__form'>
-                                        <form onSubmit={this.handleSubmit}>
-                                            <label>
-                                                Zip Code
-                                                <input type='text' onChange={this.handleChange} />
-                                            </label>
-                                            <input type='submit' value='Submit' />
-                                        </form>
-                                    </div>
-                                ) : null
-                            }
-                            locationLink={
-                                <span className='nav__link' onClick={this.handleClick}>
-                                    Location <span style={{ color: 'magenta' }}>{this.state.zipCode}</span>
-                                </span>
-                            }
-                        />
-                    </header>
-                    <main className='app__main'>
-                        <Route path='/radar' component={Radar} />
-                        <Route path='/current' render={props => <Current {...props} zipCode={this.state.zipCode} />} />
-                        <Route path='/forecast' component={Forecast} />
-                    </main>
-                    <footer className='app_footer' />
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className='app'>
+        <div className='app__container'>
+          <header className='app__header'>
+            <Nav
+              zipCode={this.state.zipCode}
+              form={
+                this.state.showMenu ? (
+                  <form className='nav__form' onSubmit={this.handleSubmit}>
+                    <label>Zip Code</label>
+                    <input
+                      type='text'
+                      pattern='[0-9]{5}'
+                      placeholder='12345'
+                      onChange={this.handleChange}
+                      required
+                      autoFocus
+                    />
+                    <input type='submit' value='Submit' />
+                  </form>
+                ) : null
+              }
+              locationLink={
+                <span className='nav__link' onClick={this.handleClick}>
+                  Location <span style={{ color: 'magenta' }}>{this.state.zipCode}</span>
+                </span>
+              }
+            />
+          </header>
+          <main className='app__main'>
+            <Route path='/radar' component={Radar} />
+            <Route path='/current' render={props => <Current {...props} zipCode={this.state.zipCode} />} />
+            <Route path='/forecast' component={Forecast} />
+          </main>
+          <footer className='app_footer' />
+        </div>
+      </div>
+    );
+  }
 }
--- src/Nav.js
+++ src/Nav.js
@@ -5,42 +5,38 @@ import { CSSTransitionGroup } from 'react-transition-group'; // ES6
 import './Nav.scss';
 
 export default class Nav extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { value: false };
-        this.handleMouseOver = this.handleMouseOver.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = { value: false };
+    this.handleMouseOver = this.handleMouseOver.bind(this);
+  }
 
-   
+  handleMouseOver() {
+    this.setState({ value: true });
+  }
 
-    handleMouseOver() {
-        this.setState({ value: true });
-    }
-
-    render() {
-    //   let menu;
-  
-    //   this.state.value ? menu = this.props.form : menu = '';
-  
-        return (
-            <nav className='nav'>
-                <div className='nav__logo'>Weather</div>
-                <div className='nav__links'>
-                    <Link className='nav__link' to='/current'>
-                        Current
-                    </Link>
-                    <Link className='nav__link' to='/forecast'>
-                        Forecast
-                    </Link>
-                    <Link className='nav__link' to='/radar'>
-                        Radar
-                    </Link>
-                    {this.props.locationLink}
-                </div>
-                <CSSTransitionGroup transitionName='example' transitionEnterTimeout={200} transitionLeaveTimeout={200}>
-                    {this.props.form}
-                </CSSTransitionGroup>
-            </nav>
-        );
-    }
+  render() {
+    return (
+      <>
+        <nav className='nav'>
+          <div className='nav__logo'>Weather</div>
+          <div className='nav__links'>
+            <Link className='nav__link' to='/current'>
+              Current
+            </Link>
+            <Link className='nav__link' to='/forecast'>
+              Forecast
+            </Link>
+            <Link className='nav__link' to='/radar'>
+              Radar
+            </Link>
+            {this.props.locationLink}
+          </div>
+        </nav>
+        <CSSTransitionGroup transitionName='example' transitionEnterTimeout={200} transitionLeaveTimeout={200}>
+          {this.props.form}
+        </CSSTransitionGroup>
+      </>
+    );
+  }
 }
