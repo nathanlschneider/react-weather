67c111c10e149cbf3fda27cf2a65c3ab31b529a3
nathanlschneider <n.l.schneider@gmail.com>
2019-02-28 18:58:52 -0500

    added spinner and box shadows

---
+++ src/Spinner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Spinner = () => {
+  return (
+    <div
+      style={{
+        width: '50px',
+        height: '50px',
+        borderRadius: '50%',
+        border: '15px solid #23232300'
+      }}
+    />
+  );
+};
+
+export default Spinner;
