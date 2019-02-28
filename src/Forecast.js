56c379c335cfdf88152df15cc197310cfc6cba82
nathanlschneider <n.l.schneider@gmail.com>
2019-02-28 02:20:54 -0500

    inital forcast formatting

--- src/Forecast.js
+++ src/Forecast.js
@@ -3,30 +3,36 @@ import Key from './KeyChain';
 import './Forecast.scss';
 
 export default class Forecast extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      forecastData: null
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            forecastData: []
+        };
+    }
 
-  static defaultProps = { zipCode: 49418 };
+    static defaultProps = { zipCode: 49418 };
 
-  componentDidMount() {
-    fetch(
-      `http://api.openweathermap.org/data/2.5/forecast?zip=${this.props.zipCode},us&units=imperial&appid=${
-        Key.openWeather
-      }`
-    )
-      .then(response => response.text())
-      .then(text => this.setState({ forecastData: text }));
-  }
+    componentDidMount() {
+        fetch(
+            `http://api.openweathermap.org/data/2.5/forecast?zip=${this.props.zipCode},us&units=imperial&appid=${
+                Key.openWeather
+            }`
+        )
+            .then(response => response.text())
+            .then(text => JSON.parse(text))
+            .then(json => this.setState({ forecastData: json.list }));
+    }
 
-  render() {
-    return (
-      <article className='forecast__article'>
-        <div className='forcast__text'>{this.state.forecastData}</div>
-      </article>
-    );
-  }
+    render() {
+        let casts = this.state.forecastData.map((datum, index) => {
+            return (
+                <>
+                    <span>{datum.weather[0].description}</span>
+                    <i key={index} className={'owf owf-3x owf-' + datum.weather[0].id} />
+                </>
+            );
+        });
+
+        return <article className='forecast__article'>{casts}</article>;
+    }
 }
